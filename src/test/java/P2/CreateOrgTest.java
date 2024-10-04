a0fbf12d472e582390186ee1c4edd143d609caf4
aliraza61199 <a.r06shorts@gmail.com>
2024-10-04 18:28:53 +0530

    massage

---
+++ src/test/java/P2/CreateOrgTest.java
@@ -0,0 +1,18 @@
+package P2;
+
+import org.testng.annotations.Test;
+
+public class CreateOrgTest {
+
+	@Test
+	public void createOrgTest() {
+
+		System.out.println("test create Org");
+	}
+	@Test
+	public void modifyOrgTest() {
+
+		System.out.println("test   modify  Org");
+	}
+
+}
---
+++ src/test/java/p1/CreateContactTest.java
@@ -0,0 +1,18 @@
+package p1;
+
+import org.testng.annotations.Test;
+
+public class CreateContactTest {
+
+	@Test
+	public void createTest() {
+
+		System.out.println("test CreateContact");
+	}
+	@Test
+	public void modifyTest() {
+
+		System.out.println("test modify Contact");
+	}
+
+}
